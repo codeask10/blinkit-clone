5f8daf69a89e2957cf6fe3a7ac869359d677b2bf
codeask10 <annajmussaquib123@gmail.com>
2024-12-02 12:53:18 +0530

    Refactor the code for the multiple images

--- src/app/components/ImageSlider.jsx
+++ src/app/components/ImageSlider.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-const ImageSlider = ({ images, image }) => {
-
-    useEffect(() => {
-        for (let i = 0; i < 2; i++) {
-            images.push("https://thumbs.dreamstime.com/b/no-photo-available-vector-icon-default-image-symbol-picture-coming-soon-web-site-mobile-app-236105298.jpg");
-        }
-    }, [images, image])
-
+const ImageSlider = ({ images, slideShow = false }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [currentIndex, images.length]);
 
     const handlePrev = () => {
         setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
@@ -20,37 +22,39 @@ const ImageSlider = ({ images, image }) => {
     };
 
     return (
-        <div className="relative w-full h-full flex justify-center items-center">
+        <div className={`relative w-full h-full flex justify-center items-center ${slideShow ? "py-8 my-4" : "w-96 h-96"}`}>
             {/* Slider Image */}
             <Image
-                className="object-cover p-8"
+                className={`object-cover  ${slideShow ? "w-full" : "w-96 h-96"}`}
                 src={
-                    images[currentIndex] ||
-                    "https://thumbs.dreamstime.com/b/no-photo-available-vector-icon-default-image-symbol-picture-coming-soon-web-site-mobile-app-236105298.jpg"
+                    images[currentIndex]?.imageUrl ? images[currentIndex].imageUrl : images[currentIndex] ||
+                        "https://thumbs.dreamstime.com/b/no-photo-available-vector-icon-default-image-symbol-picture-coming-soon-web-site-mobile-app-236105298.jpg"
                 }
                 alt="Product Image"
-                width={500}
-                height={500}
-                sizes="50vw"
+                width="0"
+                height="0"
+                sizes="100vw"
             />
 
             {/* Navigation Controls */}
-            <button
+            {images.length > 1 && <><button
                 className="absolute left-8 top-1/2 transform -translate-y-1/2  text-2xl text-slate p-2 rounded-full"
                 onClick={handlePrev}
             >
                 &lt; {/* Left Arrow */}
             </button>
-            <button
-                className="absolute right-8 top-1/2 transform -translate-y-1/2 text-2xl  text-slate p-2 rounded-full"
-                onClick={handleNext}
-            >
-                &gt; {/* Right Arrow */}
-            </button>
+                <button
+                    className="absolute right-8 top-1/2 transform -translate-y-1/2 text-2xl  text-slate p-2 rounded-full"
+                    onClick={handleNext}
+                >
+                    &gt; {/* Right Arrow */}
+                </button>
+
+            </>}
 
             {/* Indicators */}
             <div className="absolute bottom-4 flex gap-2">
-                {images.map((_, index) => (
+                {images?.length > 1 && images.map((_, index) => (
                     <div
                         key={index}
                         className={`w-2 h-2 rounded-full ${index === currentIndex ? "bg-gray-700" : "bg-gray-300"}`}
