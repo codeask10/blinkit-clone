2d055121fca470ad70a98463fd32d884fa91df48
codeask10 <annajmussaquib123@gmail.com>
2024-12-02 12:17:51 +0530

    Add scrollable Image

--- src/app/components/Collections.js
+++ src/app/components/Collections.js
@@ -1,31 +1,29 @@
 "use-client";
 import React from "react";
 import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
 
 const Collections = ({ collections }) => {
   return (
-    <div className=" my-10">
-      <h1 className="text-2xl font-medium">Top Collections</h1>
-      <div className="flex gap-2 h-60 my-5">
-        {collections?.length > 0 &&
-          collections.map(({ category }) => (
-            <div key={category.id} className="w-1/3">
-              <div className=" h-40">
-                <Image
-                  src={category.image}
-                  width="0"
-                  height="0"
-                  sizes="100vw"
-                  alt={category.name}
-                  className="w-full h-full "
-                />
-              </div>
-              <h1 className="text-xl font-base p-5 text-center">
-                {category.name}
-              </h1>
+    <div className="flex gap-4 my-5  overflow-x-scroll scrollbar-hidden">
+      {collections?.length > 0 &&
+        collections.map((collection) => (
+          <div
+            key={uuidv4()}
+            className={`flex-shrink-0 bg-gray-100 rounded-lg `}
+          >
+            <div className="p-4">
+              <Image
+                src={collection?.imageUrl}
+                width="0"
+                height="0"
+                sizes="100vw"
+                alt="Image Collection"
+                className={`object-cover w-40 h-40 rounded-lg`}
+              />
             </div>
-          ))}
-      </div>
+          </div>
+        ))}
     </div>
   );
 };
